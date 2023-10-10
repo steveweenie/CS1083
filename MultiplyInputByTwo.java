6077368b4c12ed17746effb27cf8ddb66c77ff00
Abraham Steve Colina <130194837+steveweenie@users.noreply.github.com>
2023-10-10 15:47:00 -0500

    Add files via upload

---
+++ MultiplyInputByTwo.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class MultiplyInputByTwo{
+  
+  public static void main(String[] args){
+    Scanner scnr = new Scanner(System.in);
+    int a = scnr.nextInt();
+    System.out.println(a*2);
+    
+  }
+  
+}
