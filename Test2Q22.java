fd988e94aee8771c04f891eb400f4d3536b3393a
Abraham Steve Colina <130194837+steveweenie@users.noreply.github.com>
2023-10-24 20:06:05 -0500

    EXAM 2 Question 2 Section 2 Study Guide

---
+++ Test2Q22.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Test2Q22 {
+   
+   public static void main(String[] args) {
+       for (int i = 0; i < 5; i++) {
+           for (int j = 0; j < i; j++) {
+               System.out.print(i+i*j+"\t");
+           }
+           System.out.println();
+           }
+       
+   }
+    
+}
