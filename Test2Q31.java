d458c4ba2eb99bae37084aa239b954eee4658bd4
Abraham Steve Colina <130194837+steveweenie@users.noreply.github.com>
2023-10-25 09:17:03 -0500

    EXAM 2 Question 3 Section 1 Study Guide

---
+++ Test2Q31.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Test2Q31 {
+   
+   public static void main(String[] args) {
+       for (int i = 0; i < 25; i++) {
+           System.out.print(i+"\t");
+           if (i % 5 == 4) {
+               System.out.println();
+           }
+           }
+       
+   }
+    
+}
